5f766571ee9c862758002cd7ab91cbf2279e1705
thorwebdev <thor@supabase.io>
2022-02-09 11:05:55 +0800

    feat: typescript project setup.

---
+++ typedoc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: '@supabase/supabase-auth-helpers',
+  out: './docs/',
+  exclude: [
+    './src/index.ts',
+  ],
+  excludeExternals: true,
+  excludePrivate: true,
+  hideGenerator: true,
+  readme: 'none'
+};
