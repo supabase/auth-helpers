2bf79a1f0eaefb32c41d58178570f62c7d754fad
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2022-02-14 16:17:29 +0800

    feat: npm release gh action (#10)
    * fix: withAuthRequired no cookie found behaviour.
    
    * feat: npm release gh action

--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-// Types
-export { User } from '@supabase/supabase-js';
-
-// Methods & Components
-export * from './react';
--- src/nextjs/utils/withAuthRequired.ts
+++ src/nextjs/utils/withAuthRequired.ts
@@ -57,52 +57,65 @@ export default function withAuthRequired(
     cookieOptions = COOKIE_OPTIONS
   } = options;
   return async (context: GetServerSidePropsContext) => {
-    if (!context.req.cookies) {
-      throw new Error('Not able to parse cookies!');
-    }
-    const access_token =
-      context.req.cookies[`${cookieOptions.name}-access-token`];
-    if (!access_token) {
-      throw new Error('No cookie found!');
-    }
+    try {
+      if (!context.req.cookies) {
+        throw new Error('Not able to parse cookies!');
+      }
+      const access_token =
+        context.req.cookies[`${cookieOptions.name}-access-token`];
+      if (!access_token) {
+        throw new Error('No cookie found!');
+      }
 
-    let user, accessToken;
-    // Get payload from cached access token.
-    const jwtUser = jwtDecoder(access_token);
-    if (!jwtUser?.exp) {
-      throw new Error('Not able to parse JWT payload!');
-    }
-    const timeNow = Math.round(Date.now() / 1000);
-    if (jwtUser.exp < timeNow) {
-      // JWT is expired, let's refresh from Gotrue
-      const response = await getUser(context, cookieOptions);
-      user = response.user;
-      accessToken = response.accessToken;
-    } else {
-      // Transform JWT and add note that it ise cached from JWT.
-      user = {
-        id: jwtUser.sub,
-        aud: null,
-        role: null,
-        email: null,
-        email_confirmed_at: null,
-        phone: null,
-        confirmed_at: null,
-        last_sign_in_at: null,
-        app_metadata: {},
-        user_metadata: {},
-        identities: [],
-        created_at: null,
-        updated_at: null,
-        'supabase-auth-helpers-note':
-          'This user payload is retrieved from the cached JWT and might be stale. If you need up to date user data, please call the `getUser` method in a server-side context!'
+      let user, accessToken;
+      // Get payload from cached access token.
+      const jwtUser = jwtDecoder(access_token);
+      if (!jwtUser?.exp) {
+        throw new Error('Not able to parse JWT payload!');
+      }
+      const timeNow = Math.round(Date.now() / 1000);
+      if (jwtUser.exp < timeNow) {
+        // JWT is expired, let's refresh from Gotrue
+        const response = await getUser(context, cookieOptions);
+        user = response.user;
+        accessToken = response.accessToken;
+      } else {
+        // Transform JWT and add note that it ise cached from JWT.
+        user = {
+          id: jwtUser.sub,
+          aud: null,
+          role: null,
+          email: null,
+          email_confirmed_at: null,
+          phone: null,
+          confirmed_at: null,
+          last_sign_in_at: null,
+          app_metadata: {},
+          user_metadata: {},
+          identities: [],
+          created_at: null,
+          updated_at: null,
+          'supabase-auth-helpers-note':
+            'This user payload is retrieved from the cached JWT and might be stale. If you need up to date user data, please call the `getUser` method in a server-side context!'
+        };
+        const mergedUser = { ...user, ...jwtUser };
+        user = mergedUser;
+        accessToken = access_token;
+      }
+
+      if (!user) {
+        throw new Error('No user found!');
+      }
+
+      let ret: any = { props: {} };
+      if (getServerSideProps) {
+        ret = await getServerSideProps(context);
+      }
+      return {
+        ...ret,
+        props: { ...ret.props, user: user, accessToken: accessToken }
       };
-      const mergedUser = { ...user, ...jwtUser };
-      user = mergedUser;
-      accessToken = access_token;
-    }
-
-    if (!user) {
+    } catch (e) {
       return {
         redirect: {
           destination: redirectTo,
@@ -110,14 +123,5 @@ export default function withAuthRequired(
         }
       };
     }
-
-    let ret: any = { props: {} };
-    if (getServerSideProps) {
-      ret = await getServerSideProps(context);
-    }
-    return {
-      ...ret,
-      props: { ...ret.props, user: user, accessToken: accessToken }
-    };
   };
 }
