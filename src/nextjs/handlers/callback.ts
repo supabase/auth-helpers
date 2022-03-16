b6fb729d4973f3b4de593bcdd2235f4130dd3d3a
Drake Costa <drake@saeris.io>
2022-03-16 06:37:41 -0700

    feat: Persist `provider_token` to HTTPOnly cookie on SIGNED_IN
    Potential solution for #40 and supabase/supabase#347.
    
    Adds a similar cookie persistence mechanism for `provider_token` to those used for `access_token` and `refresh_token` in src/nextjs/handlers/callback.ts
    
    Additionally, this cookie is cleared on SIGNED_OUT in both src/nextjs/handlers/callback.ts and src/nextjs/handlers/logout.ts
    
    Finally, a usage example was added to src/nextjs/README.md

--- src/nextjs/handlers/callback.ts
+++ src/nextjs/handlers/callback.ts
@@ -11,6 +11,8 @@ export interface HandleCallbackOptions {
   cookieOptions?: CookieOptions;
 }
 
+type AuthCookies = Parameters<typeof setCookies>[2];
+
 export default function handelCallback(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -31,22 +33,30 @@ export default function handelCallback(
       new NextResponseAdapter(res),
       [
         { key: 'access-token', value: session.access_token },
-        { key: 'refresh-token', value: session.refresh_token }
-      ].map((token) => ({
-        name: `${cookieOptions.name}-${token.key}`,
-        value: token.value,
-        domain: cookieOptions.domain,
-        maxAge: cookieOptions.lifetime ?? 0,
-        path: cookieOptions.path,
-        sameSite: cookieOptions.sameSite
-      }))
+        { key: 'refresh-token', value: session.refresh_token },
+        session.provider_token
+          ? { key: 'provider-token', value: session.provider_token }
+          : null
+      ].reduce<AuthCookies>((acc, token) => {
+        if (token) {
+          acc.push({
+            name: `${cookieOptions.name}-${token.key}`,
+            value: token.value,
+            domain: cookieOptions.domain,
+            maxAge: cookieOptions.lifetime ?? 0,
+            path: cookieOptions.path,
+            sameSite: cookieOptions.sameSite
+          });
+        }
+        return acc;
+      }, [])
     );
   }
   if (event === 'SIGNED_OUT') {
     setCookies(
       new NextRequestAdapter(req),
       new NextResponseAdapter(res),
-      ['access-token', 'refresh-token'].map((key) => ({
+      ['access-token', 'refresh-token', 'provider-token'].map((key) => ({
         name: `${cookieOptions.name}-${key}`,
         value: '',
         maxAge: -1
