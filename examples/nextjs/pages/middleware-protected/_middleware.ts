c073ca76fd2abdaf6e5d88bbce2e0918bde26e58
Nicolai Bjerre Pedersen <mr.bjerre@gmail.com>
2022-05-30 19:35:50 +0200

    Simplify example and add docs

--- examples/nextjs/pages/middleware-protected/_middleware.ts
+++ examples/nextjs/pages/middleware-protected/_middleware.ts
@@ -1,31 +1,9 @@
 import { withMiddlewareAuth } from '@supabase/supabase-auth-helpers/nextjs/middleware';
-import { createClient } from '@supabase/supabase-js';
-
-const getServerClientWithEnvCheck = () => {
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.SERVICE_ROLE_KEY) {
-    throw new Error(
-      'NEXT_PUBLIC_SUPABASE_URL and SERVICE_ROLE_KEY env variables are required!'
-    );
-  }
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SERVICE_ROLE_KEY!,
-    { autoRefreshToken: false, persistSession: false }
-  );
-};
-
-const serverClient = getServerClientWithEnvCheck();
 
 export const middleware = withMiddlewareAuth({
+  redirectTo: "/login",
   permissionGuard: {
-    isPermitted: async (user) => {
-      const { data } = await serverClient
-        .from('admin')
-        .select('*')
-        .eq('id', user.id)
-        .single();
-      return !!data;
-    },
+    isPermitted: async (user) => user.email?.endsWith('@example.com') || false,
     redirectTo: '/insufficient-permissions'
   }
 });
--- src/nextjs/README.md
+++ src/nextjs/README.md
@@ -272,3 +272,19 @@ import { withMiddlewareAuth } from '@supabase/supabase-auth-helpers/nextjs/middl
 
 export const middleware = withMiddlewareAuth({ redirectTo: '/login' });
 ```
+
+It is also possible to add finer granularity based on the user logged in. I.e. you can specify a promise to determine if a specific user has permission or not.
+
+
+```ts
+// pages/protected/_middleware.ts
+import { withMiddlewareAuth } from '@supabase/supabase-auth-helpers/nextjs/middleware';
+
+export const middleware = withMiddlewareAuth({ 
+  redirectTo: '/login',
+  permissionGuard: {
+    isPermitted: async (user) => user.email?.endsWith('@example.com') || false,
+    redirectTo: '/insufficient-permissions'
+  }
+});
+```
