5577a532609642e303e962ba7d50176238027f1d
GHOST <ghostdevbusiness@gmail.com>
2022-05-31 01:43:47 +0100

    move to svelte-kit package

--- examples/sveltekit/src/routes/__layout.svelte
+++ examples/sveltekit/src/routes/__layout.svelte
@@ -2,7 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { session } from '$app/stores';
 	import { supabaseClient } from '$lib/db';
-	import UserContext from '@supabase/auth-helpers-svelte';
+	import { UserContext } from '@supabase/auth-helpers-svelte';
 
 	const cbRedirect = async (user) => {
 		await goto('/');
---
+++ packages/svelte/src/index.ts
@@ -0,0 +1,4 @@
+export * from './helpers.js';
+export * from './store.js';
+
+export { default as UserContext } from './UserContext.svelte';
