5659263bb3adf236bb996eaa325f5f8de4b8e0eb
Nicolai Bjerre Pedersen <mr.bjerre@gmail.com>
2022-06-07 21:24:04 +0200

    Add permission guard to midddleware
    Add redirectTo default if not defined for guard
    
    Simplify example and add docs

--- examples/nextjs/pages/middleware-protected/_middleware.ts
+++ examples/nextjs/pages/middleware-protected/_middleware.ts
@@ -1,3 +1,9 @@
 import { withMiddlewareAuth } from '@supabase/auth-helpers-nextjs/dist/middleware';
 
-export const middleware = withMiddlewareAuth();
+export const middleware = withMiddlewareAuth({
+  redirectTo: '/login',
+  permissionGuard: {
+    isPermitted: async (user) => user.email?.endsWith('@example.com') || false,
+    redirectTo: '/insufficient-permissions'
+  }
+});
