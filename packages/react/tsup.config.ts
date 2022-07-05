423f674bd9438685caa81056a9516045ef9a4929
Andrew Smith <a.smith@silentworks.co.uk>
2022-07-05 01:46:05 +0000

    Remove comment from tsup

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -3,7 +3,6 @@ import type { Options } from 'tsup';
 export const tsup: Options = {
   dts: true,
   entryPoints: ['src/index.tsx'],
-  // `aws-amplify` is external, but sub-dependencies weren't automatically externalized ("require" statements were included)
   external: ['react', /^@supabase\//],
   format: ['cjs'],
   //   inject: ['src/react-shim.js'],
