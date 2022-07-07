74dd44edc2625782b364a6c5fb50562127904d33
David Plugge <git@davidplugge.de>
2022-07-07 16:18:52 +0200

    add ENDPOINT_PREFIX constant

--- packages/shared/src/utils/constants.ts
+++ packages/shared/src/utils/constants.ts
@@ -9,3 +9,4 @@ export const COOKIE_OPTIONS = {
 export const TOKEN_REFRESH_MARGIN = 10; // in seconds
 export const RETRY_INTERVAL = 2; // in hundred ms (initial retry after 200ms with exponential backoff)
 export const MAX_RETRIES = 10;
+export const ENDPOINT_PREFIX = '/api/auth';
