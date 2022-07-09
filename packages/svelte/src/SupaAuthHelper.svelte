79843a34c29787ed5650a5024666130a9e2fb2e5
David Plugge <git@davidplugge.de>
2022-07-09 10:22:32 +0200

    make use of constant endpoint prefix

--- packages/svelte/src/SupaAuthHelper.svelte
+++ packages/svelte/src/SupaAuthHelper.svelte
@@ -5,11 +5,14 @@
   import { checkSession, type Session } from './helpers';
   import { setIsLoading, setError, user, accessToken } from './store';
   import { dequal } from 'dequal';
-  import { CallbackUrlFailed } from '@supabase/auth-helpers-shared';
+  import {
+    CallbackUrlFailed,
+    ENDPOINT_PREFIX
+  } from '@supabase/auth-helpers-shared';
 
   // Props
   export let supabaseClient: SupabaseClient;
-  export let endpointPrefix = '/api/auth';
+  export let endpointPrefix = ENDPOINT_PREFIX;
   export let callbackUrl = `${endpointPrefix}/callback`;
   export let profileUrl = `${endpointPrefix}/user`;
   export let autoRefreshToken = true;
