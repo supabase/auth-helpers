840a75ff2ec4237992ad723f91eb012b48345114
Andrew Smith <a.smith@silentworks.co.uk>
2022-07-12 14:12:07 +0000

    Add Typescript typings to examples

--- examples/sveltekit-email-password/src/routes/dashboard.ts
+++ examples/sveltekit-email-password/src/routes/dashboard.ts
@@ -1,13 +1,24 @@
-import { supabaseServerClient, withApiAuth } from '@supabase/auth-helpers-sveltekit';
+import type { RequestHandler } from './__types/dashboard';
+import { supabaseServerClient, withApiAuth, type User } from '@supabase/auth-helpers-sveltekit';
 
-export const get = async ({ locals, request }) =>
+interface TestTable {
+	id: string;
+	created_at: string;
+}
+
+interface GetOutput {
+	user: User;
+	data: TestTable[];
+}
+
+export const get: RequestHandler<GetOutput> = async ({ locals, request }) =>
 	withApiAuth(
 		{
 			redirectTo: '/',
 			user: locals.user
 		},
 		async () => {
-			const { data } = await supabaseServerClient(request).from('test').select('*');
+			const { data } = await supabaseServerClient(request).from<TestTable>('test').select('*');
 			return {
 				body: {
 					data,
