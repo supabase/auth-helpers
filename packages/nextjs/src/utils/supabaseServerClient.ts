b64d7e58e74d5fc971a7a183cc1283d0d4d11840
Jonas Strassel <info@jonas-strassel.de>
2022-07-28 20:49:51 +0000

    fix: use primitive type for cookies object

--- packages/nextjs/src/utils/supabaseServerClient.ts
+++ packages/nextjs/src/utils/supabaseServerClient.ts
@@ -1,6 +1,7 @@
 import { GetServerSidePropsContext, NextApiRequest } from 'next';
 import { supabaseClient, SupabaseClient } from './initSupabase';
 import { CookieOptions } from '@supabase/auth-helpers-shared';
+import { NextRequest } from 'next/server';
 
 /**
  * This is a helper method to wrap your SupabaseClient to inject a user's access_token to make use of RLS on the server side.
@@ -36,8 +37,8 @@ export default function supabaseServerClient(
   if (!context.req.cookies) {
     return supabaseClient;
   }
-  const { value: access_token } =
-    context.req.cookies.getWithOptions(`${cookieOptions.name}-access-token`);
+  const access_token =
+    context.req.cookies[`${cookieOptions.name}-access-token`];
   if (access_token)
     supabaseClient.auth.setAuth(access_token);
   return supabaseClient;
