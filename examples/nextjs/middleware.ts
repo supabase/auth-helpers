adc64043fbdc2a2f3a1261ede9c3211edcf0f5e2
Nick Brinser <nickbrinser@gmail.com>
2022-08-14 13:51:29 -0500

    fix: re-add authGuard

--- examples/nextjs/middleware.ts
+++ examples/nextjs/middleware.ts
@@ -1,6 +1,12 @@
 import { withMiddlewareAuth } from '@supabase/auth-helpers-nextjs';
 
-export const middleware = withMiddlewareAuth({ redirectTo: '/' });
+export const middleware = withMiddlewareAuth({
+  redirectTo: '/',
+  authGuard: {
+    isPermitted: async (user) => user.email?.endsWith('@example.com') ?? false,
+    redirectTo: '/insufficient-permissions'
+  }
+});
 
 export const config = {
   matcher: ['/middleware-protected/:path*']
