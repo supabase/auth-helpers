b28f0e996e5d4ae0e135f8b41aa9649d645d6aa4
Nick Brinser <nickbrinser@gmail.com>
2022-08-14 15:36:19 -0500

    fix: removes svelte pkg bump, as there were no changes

--- packages/sveltekit/src/constants.ts
+++ packages/sveltekit/src/constants.ts
@@ -1,2 +1,2 @@
-export const PKG_NAME = "@supabase/auth-helpers-sveltekit";
-export const PKG_VERSION = "0.6.10";
+export const PKG_NAME = '@supabase/auth-helpers-sveltekit';
+export const PKG_VERSION = '0.6.9';
