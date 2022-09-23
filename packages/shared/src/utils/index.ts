82b94b57469d0b0112ce199c81f65e3a0852c9ca
David Plugge <git@davidplugge.de>
2022-09-23 08:28:46 +0200

    custom jwt decoder

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -14,6 +14,7 @@ export {
   CallbackUrlFailed,
   CookieNotParsed,
   JWTPayloadFailed,
+  JWTInvalid,
   RefreshTokenNotFound,
   ProviderTokenNotFound,
   type ErrorPayload
--- packages/shared/src/utils/jwt.ts
+++ packages/shared/src/utils/jwt.ts
@@ -1,3 +1,21 @@
-import { decodeJwt } from 'jose';
+import { JWTPayload } from '../types';
+import { JWTInvalid, JWTPayloadFailed } from './errors';
 
-export const jwtDecoder = (jwt: string) => decodeJwt(jwt);
+const decoder = new TextDecoder();
+
+export function jwtDecoder(jwt: string): JWTPayload {
+  const { 1: payload, length } = jwt.split('.');
+
+  if (length !== 3) {
+    throw new JWTInvalid();
+  }
+
+  try {
+    const decoded = Buffer.from(payload, 'base64');
+    const result = JSON.parse(decoder.decode(decoded));
+
+    return result;
+  } catch (err) {
+    throw new JWTPayloadFailed();
+  }
+}
