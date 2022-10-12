a3069198335b3aa061badc23bc318ed3a06349d5
Andrew Smith <a.smith@silentworks.co.uk>
2022-10-12 23:56:33 +0000

    Remove old files from 0.7.x

--- packages/sveltekit/src/server/index.ts
+++
@@ -1,6 +0,0 @@
-export { deleteSession, saveSession } from './utils/cookies.js';
-export { getProviderToken } from './utils/getProviderToken.js';
-
-export { session, getSupabaseSession } from './handlers/session.js';
-export { callback, handleCallbackSession } from './handlers/callback.js';
-export { auth } from './handlers/auth.js';
--- packages/sveltekit/src/server/utils/getProviderToken.ts
+++
@@ -1,7 +0,0 @@
-import type { Cookies } from '@sveltejs/kit';
-import { getConfig } from '../../config.js';
-
-export function getProviderToken(cookies: Cookies) {
-  const { cookieOptions } = getConfig();
-  return cookies.get(`${cookieOptions.name}-provider-token`);
-}
