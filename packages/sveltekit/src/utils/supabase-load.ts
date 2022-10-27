70cd017294bb6e4a024997227270bc0c286a9aa8
David Plugge <git@davidplugge.de>
2022-10-27 14:27:46 +0200

    sveltekit ssr disable auto token refresh

--- packages/sveltekit/src/utils/supabase-load.ts
+++ packages/sveltekit/src/utils/supabase-load.ts
@@ -14,6 +14,7 @@ export function getLoadSupabaseClient(event: LoadEvent): TypedSupabaseClient {
   return createClient(supabaseUrl, supabaseKey, {
     ...options,
     auth: {
+      autoRefreshToken: false,
       storage: {
         async getItem(key) {
           let session: Session | null = null;
