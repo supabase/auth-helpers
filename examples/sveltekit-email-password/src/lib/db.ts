ca4b1dd6025693c65625518611600127f54f00d9
René Fournier <renefournier@gmail.com>
2022-11-02 13:31:41 -0600

    Environment variables now available in builds
    Prior code means variables are not available when statically built and/or deployed to Netlify or Vercel. Otherwise, the variables are only available client-side when `npm run dev`.

--- examples/sveltekit-email-password/src/lib/db.ts
+++ examples/sveltekit-email-password/src/lib/db.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const supabaseClient = createClient(
-	env.PUBLIC_SUPABASE_URL ?? '',
-	env.PUBLIC_SUPABASE_ANON_KEY ?? ''
+	PUBLIC_SUPABASE_URL ?? '',
+	PUBLIC_SUPABASE_ANON_KEY ?? ''
 );
