7e2ec71acc10c0efc5f3f88f6ad5c9a4e9d08db7
TheDeveloper <mail@thdvlpr.com>
2022-11-24 22:08:45 +0000

    cookie

--- packages/qwik/src/index.ts
+++ packages/qwik/src/index.ts
@@ -1,15 +1,12 @@
-import type { SupabaseClient } from '@supabase/supabase-js'
+export type { Session, User, SupabaseClient } from '@supabase/supabase-js';
+
 import {
   CookieOptions,
   createBrowserSupabaseClient as _createBrowserSupabaseClient,
-  createServerSupabaseClient as _createServerSupabaseClient
+  createServerSupabaseClient as _createServerSupabaseClient,
+  parseCookies,
+  serializeCookie
 } from '@supabase/auth-helpers-shared';
-
-import { useContext, QRL } from '@builder.io/qwik'
-import { RequestEvent } from '@builder.io/qwik-city'
-import { SupabaseContext } from './components/SupabaseProvider'
-
-export type { Session, User, SupabaseClient } from '@supabase/supabase-js';
 
 export function createBrowserSupabaseClient<
   Database = any,
@@ -34,7 +31,8 @@ export function createServerSupabaseClient<
     ? 'public'
     : string & keyof Database
 >(supabaseUrl, supabaseKey,
-  event: RequestEvent,
+  context:
+    { req: { params: any, headers: any }, res: { headers: any }},
   {
     cookieOptions
   }: {
@@ -44,33 +42,33 @@ export function createServerSupabaseClient<
   return _createServerSupabaseClient<Database, SchemaName>({
     supabaseUrl,
     supabaseKey,
+    getCookie(name) {
+      const cookie = context.req.headers.get('cookie');
+      const parsed = parseCookies(cookie);
+      return parsed[name];
+    },
+    setCookie(name, value, options) {
+      const newSessionStr = serializeCookie(name, value, {
+        ...options,
+        // Allow supabase-js on the client to read the cookie as well
+        httpOnly: false
+      });
+
+      context.req.headers.append('cookie', newSessionStr);
+      context.res.headers.set('set-cookie', newSessionStr);
+    },
     getRequestHeader: (key) => {
-      return event.request.headers.get(key);
+      return context.req.headers.get(key)
     },
     getResponseHeader: (key) => {
-      const header = event.response.headers.get(key);
+      const header = context.res.getHeader(key);
       if (typeof header === 'number') {
         return String(header);
       }
 
       return header;
     },
-    setHeader: (key, value: string) => event.response.headers.set(key, value),
+    setHeader: (key, value) => context.res.setHeader(key, value),
     cookieOptions
   });
-}
-
-export type QRLSupaBaseAuth = QRL<() => Promise<SupabaseClient['auth']>>
-export type QRLSupaBase = QRL<() => SupabaseClient>
-
-export function useSupabase(): QRLSupaBase {
-  const ctx = useContext(SupabaseContext)
-
-  if (!ctx) {
-    throw new Error(
-      'useSupabase must be used within a component tree that calls useContextProvider'
-    )
-  }
-
-  return ctx
 }
