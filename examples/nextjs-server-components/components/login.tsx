6cb685059e9f41322ff91906fe1fd8e10a35c972
Jon Meyers <jonathon.d.meyers@gmail.com>
2022-12-07 10:52:21 +1100

    use global session in login component

--- examples/nextjs-server-components/components/login.tsx
+++ examples/nextjs-server-components/components/login.tsx
@@ -4,7 +4,7 @@ import { useSupabase } from './supabase-provider';
 
 // Supabase auth needs to be triggered client-side
 export default function Login() {
-  const { supabase } = useSupabase();
+  const { supabase, session } = useSupabase();
 
   const handleEmailLogin = async () => {
     const { error } = await supabase.auth.signInWithPassword({
@@ -35,11 +35,15 @@ export default function Login() {
     }
   };
 
-  return (
+  // this `session` is from the root loader - server-side
+  // therefore, it can safely be used to conditionally render
+  // SSR pages without issues with hydration
+  return session ? (
+    <button onClick={handleLogout}>Logout</button>
+  ) : (
     <>
       <button onClick={handleEmailLogin}>Email Login</button>
       <button onClick={handleGitHubLogin}>GitHub Login</button>
-      <button onClick={handleLogout}>Logout</button>
     </>
   );
 }
