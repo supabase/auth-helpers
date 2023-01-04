65f9f6d5c1cc85f16bb4ec870fdd12bfb5474f4e
Andrew Smith <a.smith@silentworks.co.uk>
2023-01-04 14:40:42 +0000

    Update example code to sveltekit 1.0

--- examples/sveltekit-email-password/src/routes/(app)/+page.server.ts
+++ examples/sveltekit-email-password/src/routes/(app)/+page.server.ts
@@ -1,10 +1,10 @@
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { AuthApiError } from '@supabase/supabase-js';
-import { invalid, redirect, type ValidationError } from '@sveltejs/kit';
+import { fail, redirect, type ActionFailure } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
-	async default(event): Promise<ValidationError<{ error: string; values?: { email: string } }>> {
+	async default(event): Promise<ActionFailure<{ error: string; values?: { email: string } }>> {
 		const { request } = event;
 		const { supabaseClient } = await getSupabase(event);
 		const formData = await request.formData();
@@ -13,12 +13,12 @@ export const actions: Actions = {
 		const password = formData.get('password') as string;
 
 		if (!email) {
-			return invalid(400, {
+			return fail(400, {
 				error: 'Please enter your email'
 			});
 		}
 		if (!password) {
-			return invalid(400, {
+			return fail(400, {
 				error: 'Please enter your password',
 				values: {
 					email
@@ -30,14 +30,14 @@ export const actions: Actions = {
 
 		if (error) {
 			if (error instanceof AuthApiError && error.status === 400) {
-				return invalid(400, {
+				return fail(400, {
 					error: 'Invalid credentials.',
 					values: {
 						email
 					}
 				});
 			}
-			return invalid(500, {
+			return fail(500, {
 				error: 'Server error. Try again later.',
 				values: {
 					email
