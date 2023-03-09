2a85f3db6815b8baf7ff44d48c4fc54e2bfba210
Andrew Smith <a.smith@silentworks.co.uk>
2023-03-09 10:42:33 +0000

    Update with +layout.svelte handling event listening (#468)

--- examples/sveltekit-magic-link/src/routes/+layout.svelte
+++ examples/sveltekit-magic-link/src/routes/+layout.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	$: ({ supabase } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => subscription.unsubscribe();
+	});
+</script>
+
 <svelte:head>
 	<title>Supabase Auth Helpers Demo</title>
 </svelte:head>
--- examples/sveltekit/src/routes/+layout.svelte
+++ examples/sveltekit/src/routes/+layout.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => subscription.unsubscribe();
+	});
+
 	function signout() {
-		data.supabase.auth.signOut();
+		supabase.auth.signOut();
 	}
 </script>
 
-{#if data.session}
+{#if session}
 	<button on:click={signout}>Sign out</button>
 {/if}
 
