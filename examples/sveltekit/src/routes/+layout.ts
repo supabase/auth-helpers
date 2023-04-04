9ddcd5c71016a97f102ef27a153910601b280cf0
David Plugge <59972093+david-plugge@users.noreply.github.com>
2023-04-04 19:17:23 +0200

    deprecate onAuthStateChange property (#472)

--- examples/sveltekit/src/routes/+layout.ts
+++ examples/sveltekit/src/routes/+layout.ts
@@ -1,4 +1,3 @@
-import { invalidate } from '$app/navigation';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { LayoutLoad } from './$types';
