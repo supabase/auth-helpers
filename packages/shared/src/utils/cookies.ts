b1d6a1bc9b10ea68c5fca1ce6d024d80be495b65
Andrew Smith <a.smith@silentworks.co.uk>
2023-04-16 18:02:41 +0000

    Add jose for cross platform base64url support

--- packages/shared/src/utils/cookies.ts
+++ packages/shared/src/utils/cookies.ts
@@ -1,6 +1,6 @@
 import { Session } from '@supabase/supabase-js';
 import { parse, serialize } from 'cookie';
-import { fromBase64 } from 'js-base64';
+import { base64url } from 'jose';
 
 export { parse as parseCookies, serialize as serializeCookie };
 
@@ -67,13 +67,14 @@ export function parseSupabaseCookie(
     }
 
     const [_header, payloadStr, _signature] = session[0].split('.');
-    const payload = fromBase64(payloadStr);
+    const payload = base64url.decode(payloadStr);
+    const decoder = new TextDecoder();
 
-    const { exp, sub, ...user } = JSON.parse(payload);
+    const { exp, sub, ...user } = JSON.parse(decoder.decode(payload));
 
     return {
       expires_at: exp,
-      expires_in: exp - Math.round(Date.now() / 1000),
+      expires_in: (exp ?? 0) - Math.round(Date.now() / 1000),
       token_type: 'bearer',
       access_token: session[0],
       refresh_token: session[1],
