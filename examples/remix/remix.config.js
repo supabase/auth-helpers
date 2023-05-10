cac4a7286f182f5c1e9623d4829915391c797f95
David Plugge <git@davidplugge.de>
2023-05-11 00:50:50 +0200

    format files

--- examples/remix/remix.config.js
+++ examples/remix/remix.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+	ignoredRouteFiles: ['**/.*']
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// serverBuildPath: "build/index.js",
+	// publicPath: "/build/",
 };
