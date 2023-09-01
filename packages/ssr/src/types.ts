52a99a0f7016689d7df8ac52e78498b60d501468
Andrew Smith <a.smith@silentworks.co.uk>
2023-09-01 01:19:29 +0000

    Fix types for cookie options

--- packages/ssr/src/types.ts
+++ packages/ssr/src/types.ts
@@ -1,8 +1,7 @@
 import type { CookieSerializeOptions } from 'cookie';
 
-export type CookieOptions = Pick<
-	CookieSerializeOptions,
-	'domain' | 'secure' | 'path' | 'sameSite' | 'maxAge'
+export type CookieOptions = Partial<
+	Pick<CookieSerializeOptions, 'domain' | 'secure' | 'path' | 'sameSite' | 'maxAge'>
 >;
 
 export type CookieOptionsWithName = { name?: string } & CookieOptions;
