94ead2964b676b58ad04372e0efa86e4b1de9716
Tom Sherman <the.tomsherman@gmail.com>
2023-09-06 11:09:12 +0100

    Wrap createServerComponentClient in React.cache

--- packages/nextjs/src/serverComponentClient.ts
+++ packages/nextjs/src/serverComponentClient.ts
@@ -6,6 +6,7 @@ import {
 	createSupabaseClient
 } from '@supabase/auth-helpers-shared';
 import { cookies } from 'next/headers';
+import { cache } from 'react';
 
 import type { SupabaseClient } from '@supabase/supabase-js';
 import type { GenericSchema } from '@supabase/supabase-js/dist/module/lib/types';
@@ -34,48 +35,50 @@ class NextServerComponentAuthStorageAdapter extends CookieAuthStorageAdapter {
 	}
 }
 
-export function createServerComponentClient<
-	Database = any,
-	SchemaName extends string & keyof Database = 'public' extends keyof Database
-		? 'public'
-		: string & keyof Database,
-	Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
-		? Database[SchemaName]
-		: any
->(
-	context: {
-		cookies: () => ReturnType<typeof cookies>;
-	},
-	{
-		supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL,
-		supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-		options,
-		cookieOptions
-	}: {
-		supabaseUrl?: string;
-		supabaseKey?: string;
-		options?: SupabaseClientOptionsWithoutAuth<SchemaName>;
-		cookieOptions?: CookieOptionsWithName;
-	} = {}
-): SupabaseClient<Database, SchemaName, Schema> {
-	if (!supabaseUrl || !supabaseKey) {
-		throw new Error(
-			'either NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY env variables or supabaseUrl and supabaseKey are required!'
-		);
-	}
-
-	return createSupabaseClient<Database, SchemaName, Schema>(supabaseUrl, supabaseKey, {
-		...options,
-		global: {
-			...options?.global,
-			headers: {
-				...options?.global?.headers,
-				'X-Client-Info': `${PACKAGE_NAME}@${PACKAGE_VERSION}`
-			}
+export const createServerComponentClient = cache(
+	<
+		Database = any,
+		SchemaName extends string & keyof Database = 'public' extends keyof Database
+			? 'public'
+			: string & keyof Database,
+		Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
+			? Database[SchemaName]
+			: any
+	>(
+		context: {
+			cookies: () => ReturnType<typeof cookies>;
 		},
-		auth: {
-			storageKey: cookieOptions?.name,
-			storage: new NextServerComponentAuthStorageAdapter(context, cookieOptions)
+		{
+			supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL,
+			supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+			options,
+			cookieOptions
+		}: {
+			supabaseUrl?: string;
+			supabaseKey?: string;
+			options?: SupabaseClientOptionsWithoutAuth<SchemaName>;
+			cookieOptions?: CookieOptionsWithName;
+		} = {}
+	): SupabaseClient<Database, SchemaName, Schema> => {
+		if (!supabaseUrl || !supabaseKey) {
+			throw new Error(
+				'either NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY env variables or supabaseUrl and supabaseKey are required!'
+			);
 		}
-	});
-}
+
+		return createSupabaseClient<Database, SchemaName, Schema>(supabaseUrl, supabaseKey, {
+			...options,
+			global: {
+				...options?.global,
+				headers: {
+					...options?.global?.headers,
+					'X-Client-Info': `${PACKAGE_NAME}@${PACKAGE_VERSION}`
+				}
+			},
+			auth: {
+				storageKey: cookieOptions?.name,
+				storage: new NextServerComponentAuthStorageAdapter(context, cookieOptions)
+			}
+		});
+	}
+);
