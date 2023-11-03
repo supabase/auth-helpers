b55161f800ec18c4ed8b1c8fec9680a1cf0d6b71
Andrew Smith <a.smith@silentworks.co.uk>
2023-11-03 22:31:00 +0000

    Update with sameSite info

--- packages/ssr/src/utils/constants.ts
+++ packages/ssr/src/utils/constants.ts
@@ -2,6 +2,6 @@ import { CookieOptions } from '../types';
 
 export const DEFAULT_COOKIE_OPTIONS: CookieOptions = {
 	path: '/',
-	maxAge: 60 * 60 * 24 * 365 * 1000,
-	httpOnly: false
+	sameSite: 'lax',
+	maxAge: 60 * 60 * 24 * 365 * 1000
 };
