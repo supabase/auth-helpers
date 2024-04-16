3eb0115e1ef6b7c58571f3caa76d4ef8f65d1c1e
Stojan Dimitrovski <sdimitrovski@gmail.com>
2024-04-16 13:56:21 +0200

    feat: auth-helpers-nextjs total fix with createMiddlewareClient2

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -3,7 +3,7 @@ export type { Session, User, SupabaseClient } from '@supabase/supabase-js';
 
 export { createPagesBrowserClient } from './pagesBrowserClient';
 export { createPagesServerClient } from './pagesServerClient';
-export { createMiddlewareClient } from './middlewareClient';
+export { createMiddlewareClient, createMiddlewareClientV2 } from './middlewareClient';
 export { createClientComponentClient } from './clientComponentClient';
 export { createServerComponentClient } from './serverComponentClient';
 export { createRouteHandlerClient } from './routeHandlerClient';
